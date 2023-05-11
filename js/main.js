788ceb5fa5ff23a8f0c4d93ee099bb760223c740
Lavínia Rodrigues <lavinia.rodriguesr@gmail.com>
2023-05-11 09:41:17 -0300

    imports finalizado

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
+import { solicitacoes } from './solicitacoes.js';
+
 function cepMascara(cep) {
     if (cep.value.length == 5) {
         cep.value = cep.value + '-' 
     }
 }
 
-const solicitacoes = [];
+const solicitacoesCards = [];
 
 function submitHandler(event) {
     event.preventDefault();
@@ -13,12 +15,12 @@ function submitHandler(event) {
   
     const novaSolicitacao = Object.fromEntries(new FormData(form));
 
-    if (solicitacoes.filter(solicitacao => solicitacao.cep == novaSolicitacao.cep).length == 0){
-        solicitacoes.push(novaSolicitacao);
+    if (solicitacoesCards.filter(solicitacao => solicitacao.cep == novaSolicitacao.cep).length == 0){
+        solicitacoesCards.push(novaSolicitacao);
         insertSolicitacaoCard(novaSolicitacao);
     }
      
-    console.log(solicitacoes);
+    console.log(solicitacoesCards);
 
     form.reset();
 }
@@ -49,17 +51,7 @@ function solicitacaoCard(solicitacao) {
             </div>`;
   }
 
-  /*<div class="row">
-            <div class="col s12 m6">
-            <div class="card">
-            <div class="card-image">
-            <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.626466790964!2d-34.861530524183976!3d-7.16910607034859!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7acc279d9d6f405%3A0xa0e0f84355a1b9cb!2sR.%20Ant%C3%B4nio%20Ximenes%20-%20Jos%C3%A9%20Am%C3%A9rico%20de%20Almeida%2C%20Jo%C3%A3o%20Pessoa%20-%20PB%2C%2058073-410!5e0!3m2!1sen!2sbr!4v1683496178263!5m2!1sen!2sbr"   allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            <span class="card-title">Cep: ${solicitacao.cep}</span>
-            </div>
-            <div class="card-content">
-            <span>Endereço:${solicitacao.endereco}</span>
-            <p>Motivo da solicitação: ${solicitacao.justificativa}</p>
-            </div>
-            </div>
-            </div>
-            </div>*/
+window.submitHandler = submitHandler;
+window.cepMascara = cepMascara;
+
+solicitacoes.forEach((solicitacao) => insertSolicitacaoCard(solicitacao));
---
+++ js/solicitacoes.js
@@ -0,0 +1,30 @@
+export const solicitacoes = [
+    {
+        nome: 'Pessoa 1',
+        email: 'pessoa1@email.com',
+        telefone: '83 99999-9999',
+        cep: '58090800',
+        endereco: 'Rua teste 1',
+        referencia: 'Próximo a rua teste',
+        justificativa: 'Muito lixo acumulado'
+    },
+    {
+        nome: 'Pessoa 2',
+        email: 'pessoa2@email.com',
+        telefone: '83 99999-9999',
+        cep: '58011402',
+        endereco: 'Rua teste 2',
+        referencia: 'Próximo a rua teste 2',
+        justificativa: 'Muito lixo acumulado'
+    },
+    {
+        nome: 'Pessoa 3',
+        email: 'pessoa3@email.com',
+        telefone: '83 99999-9999',
+        cep: '58110001',
+        endereco: 'Rua teste 2',
+        referencia: 'Próximo a rua teste 2',
+        justificativa: 'Muito lixo acumulado'
+    },
+  ];
+  
